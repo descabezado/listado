a5b9572a6aed4471058eafd7e24ad75dddbad3e3
Wilfrido Escorcia <ingescorcia65@gmail.com>
2023-08-23 21:20:08 -0500

    app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import ToDoForm from './components/ToDoForm.component';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Grupo 8 </h1>
+        <ul>
+          <li>Nahomi Valentina</li>
+          <li>Stephany Chacón</li>
+          <li>Wilfrido Escorcia</li>
+        </ul>
+      </div>
+      <h1>To Do List</h1>
+      <ToDoForm/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
